a3a37a5c5395efd2abbc1d41fc8ba905ed000fa8
Alexander <algurev0220@gmail.com>
2025-05-04 15:30:31 +0300

    RestController

--- src/main/java/telran/books/book/controller/BookController.java
+++ src/main/java/telran/books/book/controller/BookController.java
@@ -1,9 +1,8 @@
 package telran.books.book.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import telran.books.book.dto.AuthorDto;
 import telran.books.book.dto.BookDto;
 import telran.books.book.service.BookService;
 
@@ -17,7 +16,40 @@ public class BookController {
         return bookService.addBook(bookDto);
     }
 
+    @GetMapping("/book/{isbn}")
+    public BookDto findBookByIsbn(@PathVariable String isbn) {
+        return bookService.findBookByIsbn(isbn);
+    }
 
+    @DeleteMapping("/book/{isbn}")
+    public BookDto removeBook(@PathVariable String isbn) {
+        return bookService.removeBook(isbn);
+    }
+
+    @PutMapping("/book/{isbn}/title/{newTitle}")
+    public BookDto updateBookTitle(@PathVariable String isbn, @PathVariable String newTitle) {
+        return bookService.updateBookTitle(isbn, newTitle);
+    }
+
+    @GetMapping("/books/author/{author}")
+    public BookDto[] findBooksByAuthor(@PathVariable String author) {
+        return bookService.findBooksByAuthor(author);
+    }
+
+    @GetMapping("/books/publisher/{publisher}")
+    public BookDto[] findBooksByPublisher(@PathVariable String publisher) {
+        return bookService.findBooksByPublisher(publisher);
+    }
+
+    @GetMapping("/authors/book/{isbn}")
+    public AuthorDto[] findBookAuthors(@PathVariable String isbn) {
+        return bookService.findBookAuthors(isbn);
+    }
+
+    @GetMapping("/publishers/author/{author}")
+    public String[] findPublishersByAuthor(@PathVariable String author) {
+        return bookService.findPublishersByAuthor(author);
+    }
 
 
 
--- src/main/java/telran/books/book/service/BookService.java
+++ src/main/java/telran/books/book/service/BookService.java
@@ -1,7 +1,22 @@
 package telran.books.book.service;
 
+import telran.books.book.dto.AuthorDto;
 import telran.books.book.dto.BookDto;
 
 public interface BookService {
     boolean addBook(BookDto bookDto);
+
+    BookDto findBookByIsbn(String isbn);
+
+    BookDto removeBook(String isbn);
+
+    BookDto updateBookTitle(String isbn, String newTitle);
+
+    BookDto[] findBooksByAuthor(String author);
+
+    BookDto[] findBooksByPublisher(String publisher);
+
+    AuthorDto[] findBookAuthors(String isbn);
+
+    String[] findPublishersByAuthor(String author);
 }
